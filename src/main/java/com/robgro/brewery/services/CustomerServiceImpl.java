2791a7495002397498199ec44c9ad634d4e2eab4
RobGro <r.grodzki74@gmail.com>
2022-04-07 20:33:38 +0100

    added POST and PUT to beer

--- src/main/java/com/robgro/brewery/services/CustomerServiceImpl.java
+++ src/main/java/com/robgro/brewery/services/CustomerServiceImpl.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 import java.util.UUID;
 
 @Service
-public class CustomerServiceImpl implements CustomerService{
+public class CustomerServiceImpl implements CustomerService {
 
     @Override
     public CustomerDto getCustomerById(UUID customerId) {
--- src/main/java/com/robgro/brewery/web/controller/BeerController.java
+++ src/main/java/com/robgro/brewery/web/controller/BeerController.java
@@ -3,12 +3,10 @@ package com.robgro.brewery.web.controller;
 import com.robgro.brewery.services.BeerService;
 import com.robgro.brewery.web.model.BeerDto;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -29,5 +27,22 @@ public class BeerController {
         log.info("beer Id is: " + beerId);
 
         return new ResponseEntity<>(beerService.getBeerById(beerId), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity handlePost(BeerDto beerDto) {
+
+        BeerDto savedDto = beerService.saveNewBeer(beerDto);
+
+        HttpHeaders headers = new HttpHeaders();
+        // todo add hostname to url --> "http://localhost:8080/api/v1/beer/"
+        headers.add("Location", "/api/v1/beer/" + savedDto.getId().toString());
+        return new ResponseEntity(headers, HttpStatus.CREATED);
+    }
+
+    @PutMapping({"/{beerId}"})
+    public ResponseEntity handleUpdate(@PathVariable("beerId") UUID beerId, BeerDto beerDto) {
+        beerService.updateBeer(beerId, beerDto);
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 }
